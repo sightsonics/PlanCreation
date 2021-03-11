6b7e36b6c44a6d5908ca404c156f0ec10ca81b97
SightSonics <71474752+sightsonics@users.noreply.github.com>
2021-03-11 09:25:29 -0800

    ScanPlan Creation
    This Program is for the automatic creation of ScanPlans for the SpotSight System.

---
+++ ScanPlanCreation/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TextWriter3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int weldNumber;
+            // File name  
+            string fileName = @"C:\SightSonics\ScanPlanCreation.ScanPlan.txt";
+            FileStream stream = null;
+            try
+            {
+                // Create a FileStream with mode CreateNew  
+                stream = new FileStream(fileName, FileMode.OpenOrCreate);
+                // Create a StreamWriter from FileStream  
+                using (StreamWriter writer = new StreamWriter(stream, Encoding.UTF8))
+                {
+                    writer.WriteLine("*--------------------*");
+                    writer.WriteLine("INSPECTION PLAN");
+                    writer.WriteLine("*---------------------*");
+                    writer.WriteLine("MODEL: 	2LL Dan Coupon Scan");
+                    writer.WriteLine("LOCATION: 	2LL Dan Coupon Scan");
+                    writer.WriteLine("PLAN: 	2LL Dan Coupon Scan");
+                    writer.WriteLine("WELD NUMBER: 	542");
+                    writer.WriteLine("\r");
+
+                    do 
+                        weldNumber += 1
+                    writer.WriteLine("WELD 1");
+                    writer.WriteLine("WELD ID: 	T6-001");
+                    writer.WriteLine(@"SETUP PATH: 	C:\Program Files (x86)\SpotSight\Support Files\Setup Files\2x2 28db 8.usc");
+                    writer.WriteLine("QUALITY LINE: 	1	4.00	0	0.00	0.00	0	0	8.00	8.00	3.200000");
+                    writer.WriteLine("NUMBER OF SHEETS: 	3");
+                    writer.WriteLine("SHEET 1: 	0.65	STEEL");
+                    writer.WriteLine("SHEET 2: 	1.80	STEELL");
+                    writer.WriteLine("SHEET 3: 	2.00	STEEL");
+                    writer.WriteLine("GATE 1: 	1.23	0.38");
+                    writer.WriteLine("GATE 2: 	1.62	0.20");
+                    writer.WriteLine("JPEG PATH: ");
+                    writer.WriteLine("ALIGNMENT FACTOR: 	0.000");
+                    weldNumber <= 542;
+                }
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Dispose();
+            }
+            // Read a file  
+            string readText = File.ReadAllText(fileName);
+            Console.WriteLine(readText);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ScanPlanCreation/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace ScanPlanCreation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Model_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Take box value and add it to text file
+            //string model = Model.Text;
+            //Model.Text = TextInput.txt;
+            Console.WriteLine(Model.Text);
+        }
+    }
+}
